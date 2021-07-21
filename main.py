5394c184fd59e69ace970f12d2ead94a8708ad90
kuwa! <83022348+kuwacom@users.noreply.github.com>
2021-07-21 12:53:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ client = Client()
 
 @client.event
 async def on_ready():
-    print(f"")
+    print(f"ミニくわです！")
 
 @client.event
 async def on_message(msg: Message):
     msg.content
-
+    for channel in client.get_all_channels():
+        if channel.name == "ミニくわ":
+            await channel.send("ミニくわ起動しました")
 client.run(os.getenv("DC_TOKEN"))
