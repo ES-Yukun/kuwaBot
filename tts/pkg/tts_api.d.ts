874f0bfa302b669e6b715b8ed4fca72a8c1959b6
Sueqkjs <sueqk@outlook.jp>
2021-08-02 05:30:21 +0000

    add

---
+++ tts/pkg/tts_api.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} text
+* @param {string} language
+* @returns {any}
+*/
+export function speak_js(text: string, language: string): any;
---
+++ tts/pkg/tts_api.js
@@ -0,0 +1,2 @@
+import * as wasm from "./tts_api_bg.wasm";
+export * from "./tts_api_bg.js";
